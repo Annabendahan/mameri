bc85a9f85d1361ffcb6149b054034f4e4faa134d
Anna Bendahan <an.bendahan@gmail.com>
2019-06-26 21:32:56 +0200

    prefix modified

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/gatsby-deploy`,
+  pathPrefix: `/mameri`,
   siteMetadata: {
     title: `quentinmameri`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
