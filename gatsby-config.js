ee8876f268cdc138bb886032eeb868ac28ce0386
Anna Bendahan <an.bendahan@gmail.com>
2019-07-05 20:47:18 +0200

    changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "annabendahan.github.io/mameri/",
+        baseUrl: "admin.qmavocats.com",
         // The protocol. This can be http or https.
         protocol: "https",
         // Indicates whether the site is hosted on wordpress.com.
