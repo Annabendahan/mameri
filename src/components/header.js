72dedb969dbba5b40de36923582655540a8daed3
Anna Bendahan <an.bendahan@gmail.com>
2019-07-05 17:31:35 +0200

    pb hono

--- src/components/header.js
+++ src/components/header.js
@@ -77,7 +77,7 @@ const Header = ({ siteTitle }) => (
         >
           DOMAINES D'EXPERTISE <svg className="polygon" width="10" height="4" viewBox="0 0 10 4" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M5 4L0.669823 0.5H9.33008L5 4Z" fill="#244C82"/>
-</svg>
+</svg> </Link>
 
 
           <span className="so sub-onglet">  <Link to="/indemnisation-des-victimes/" > INDEMNISATION DES VICTIMES <svg width="5" height="7" viewBox="0 0 5 10" fill="none" xmlns="http://www.w3.org/2000/svg">
