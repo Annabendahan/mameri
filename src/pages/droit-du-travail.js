6038e48833912bd664cc5b25abc6c97debe07def
Anna Bendahan <an.bendahan@gmail.com>
2019-08-28 23:16:25 +0200

    last changes

--- src/pages/droit-du-travail.js
+++ src/pages/droit-du-travail.js
@@ -4,6 +4,7 @@ import '../components/contact.css'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import '../components/footer.css'
+import VisitCard from '../components/VisitCard'
 import GoogleMap from "../components/maps"
 import MapContainer from "../components/maps2"
 import pic2 from '../images/quentinmameripic.png';
@@ -24,17 +25,17 @@ const Droittravail = () => (
 
 <p className="desc"> Cela implique que le salarié puisse justifier de manquements graves de son employeur rendant impossible la poursuite de la relation de travail.
 
-La procédure en la matière se déroule en plusieurs étapes :
+La procédure en la matière se déroule en plusieurs étapes : </p>
 
-<span className="bold"> Courrier adressé à l’employeur prenant acte de la rupture de ton contrat de travail. </span> Cela a pour effet de rompre immédiatement le contrat de travail sans que le salarié soit tenu d’effectuer un préavis. Dès que le courrier de prise d’acte a été envoyé à l’employeur, le contrat prend fin de manière immédiate sans rétractation possible.
+<p className="desc"> <p className="desc"> <span className="bold"> Courrier adressé à l’employeur prenant acte de la rupture de ton contrat de travail. </span> Cela a pour effet de rompre immédiatement le contrat de travail sans que le salarié soit tenu d’effectuer un préavis. Dès que le courrier de prise d’acte a été envoyé à l’employeur, le contrat prend fin de manière immédiate sans rétractation possible. </p>
 
-<span className="bold"> Saisine du bureau de jugement du Conseil des Prud’hommes </span>  afin de solliciter la requalification de cette rupture en licenciement aux torts de l’employeur.
+<p className="desc"> <span className="bold"> Saisine du bureau de jugement du Conseil des Prud’hommes </span>  afin de solliciter la requalification de cette rupture en licenciement aux torts de l’employeur. </p>
 
 A l’issue de la procédure, plusieurs cas de figures peuvent se présenter :
 
 <p className="desc"> - Si le Conseil des Prud’homme considère que les manquements reprochés à l’employeur sont caractérisés et suffisamment graves pour faire obstacle à la poursuite du contrat de travail, la prise d’acte sera requalifiée en licenciement sans cause réelle et sérieuse ce qui ouvrira droit au versement par l’employeur des indemnités afférentes au licenciement (indemnité légale ou conventionnelle de licenciement, indemnité de congés payés et de préavis) ainsi qu’à l’allocation de dommages et intérêts pour licenciement sans cause réelle et sérieuse.</p>
 
-- Dans le cas contraire, la prise d’acte sera requalifiée en démission. Le salarié sera alors privé d’indemnités afférentes au licenciement et de dommages et intérêts et pourra être condamné à régler à son employeur une indemnité compensatrice de préavis.
+<p className="desc"> - Dans le cas contraire, la prise d’acte sera requalifiée en démission. Le salarié sera alors privé d’indemnités afférentes au licenciement et de dommages et intérêts et pourra être condamné à régler à son employeur une indemnité compensatrice de préavis.
 
 Eu égard aux risques inhérents à la prise d’acte, il est essentiel pour un salarié de prendre conseil auprès d’un avocat afin que celui-ci puisse le conseiller utilement sur l’opportunité d’une telle procédure et constituer un dossier solide et suffisamment étayé.
 
@@ -56,6 +57,91 @@ Si le juge estime le(s) manquement(s) invoqué(s) suffisamment grave, le contrat
 Si au contraire, le juge n’estime pas le(s) manquement(s) invoqué(s) suffisamment grave, le contrat n’est pas résilié et il conviendra, dans ce cas d’envisager, un autre mode de rupture puisque dans ce cas, le salarié fera toujours partie des effectifs de l’entreprise.
 
 Pour toute information complémentaire, vous pouvez contacter Maitre Quentin MAMERI, avocat de victime à Paris, par téléphone, mail ou par le formulaire de contact afin de lui exposer votre situation et convenir d’un rendez-vous. </p>
+
+
+
+
+
+ <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> RUPTURE CONVENTIONNELLE </h2>
+
+<p className="desc"> La rupture conventionnelle est un mode de rupture du contrat de travail.
+Elle permet à l’employeur et au salarié de mettre fin, d’un commun accord, au
+contrat de travail à durée indéterminée qui les lie à des conditions qu’ils déterminent
+ensemble.</p>
+
+<p className="desc">Cet accord est formalisé par une convention qui est négociée au cours d’un ou
+plusieurs entretiens préalables obligatoires au cours desquels le salarié peut être
+assisté par un membre de l’entreprise, d’un membre syndical ou d’une institution
+représentative du personnel ou, à défaut de syndicat ou d’institutions
+représentatives dans l’entreprise, par un conseiller du salarié inscrit sur une liste
+auprès de la préfecture.</p>
+<p className="desc"> A compter de la signature de la convention, chaque partie dispose d’un délai de 15
+jours pour se rétracter.</p>
+<p className="desc"> A l’issue de ce délai, l’employeur ou le salarié adressent la convention auprès de la
+Direccte pour homologation. La Direccte dispose d’un délai de 15 jours pour se
+prononcer. A défaut, l’homologation est acquise.</p>
+
+
+
+<p className="desc">Vous pouvez contacter Maitre Quentin MAMERI, par téléphone, mail ou par le
+formulaire de contact afin de lui exposer votre situation et convenir d’un rendez-
+vous.</p>
+
+
+
+ <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> INAPTITUDE </h2>
+
+
+
+<p className="desc"> <span className="bold">Le médecin du travail peut se prononcer sur l’aptitude ou l’inaptitude du salarié
+dans le cadre d’une visite de reprise après une période d’au moins 30 jours d’arrêt
+maladie ou à tout moment à l’initiative de l’employeur, du salarié ou de la
+médecine du travail. </span></p>
+<p className="desc">En cas d’inaptitude, cela n’est pas sans conséquence pour le salarié. L’employeur
+peut soit procéder à son reclassement par aménagement, transformation ou
+changement de poste, soit procéder à son licenciement pour inaptitude.</p>
+<p className="desc">Si vous êtes en désaccord avec les avis, propositions, conclusions écrites ou
+indications émis par le médecin du travail, vous pouvez les contester devant le
+Conseil de prud’hommes dans un délai de 15 jours à compter de la notification de la
+décision.
+Ce délai est très court, soyez donc très vigilant !</p>
+<p className="desc" id="H">Si vous êtes en accord avec la position du médecin du travail mais que vous contestez
+le licenciement prononcé par votre employeur, vous pouvez saisir le Conseil de
+Prud’hommes.</p>
+<p className="desc">Vous pouvez contacter Me Quentin MAMERI par téléphone, mail ou par le formulaire
+de contact afin d’évoquer votre situation et de convenir d’un rendez-vous.</p>
+
+
+
+
+ <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> HARCÈLEMENT MORAL </h2>
+
+
+  <p className="desc"> <span className="bold"> Le harcèlement moral correspond à des agissements répétés exercés par la
+hiérarchie ou non à l’encontre d’un salarié qui ont pour objet ou pour effet une
+dégradation des conditions de travail susceptible de porter atteinte à ses droits et à
+sa dignité, d'altérer sa santé physique ou mentale ou de compromettre son avenir
+professionnel. </span></p>
+<p className="desc">Le harcèlement moral peut prendre plusieurs formes comme, notamment, des
+brimades, des injures, des violences, une procédure disciplinaire injustifiée, une
+« placardisation » … </p>
+<p className="desc">En cas de recours, il vous appartient uniquement de présenter les éléments de fait
+laissant supposer l’existence d’un harcèlement moral. Votre employeur devra
+démontrer que les faits allégués ne correspondent pas une situation de harcèlement
+moral.</p>
+
+
+<p className="desc">Si vous êtes victime de faits de harcèlement moral, vous pouvez contacter Me
+Quentin MAMERI par téléphone, mail ou par le formulaire de contact afin d’évoquer
+votre situation et de convenir d’un rendez-vous.</p>
+
+
 
     </div>
 
--- src/pages/reparation-dommage-corporel.js
+++ src/pages/reparation-dommage-corporel.js
@@ -4,6 +4,7 @@ import '../components/liens.css'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import '../components/footer.css'
+import VisitCard from '../components/VisitCard'
 import GoogleMap from "../components/maps"
 import MapContainer from "../components/maps2"
 import pic2 from '../images/quentinmameripic.png';
@@ -33,6 +34,179 @@ class Reparation extends  Component {
      <div className="bigbox">
       <div className="bleft">
       <h1> RÉPARATION DU DOMMAGE CORPOREL </h1>
+
+
+
+      <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> LE DOSSIER MEDICAL  </h2>
+
+ <p className="desc"> <span className="bold"> <Link to="/conseils-aux-victimes">  Voir comment solliciter votre dossier médical  </Link> </span> </p>
+
+
+
+<h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> LA CONSOLIDATION </h2>
+
+
+   <p className="desc"> La consolidation est une notion médico-légale essentielle dans l’indemnisation du dommage
+corporel. </p>
+
+
+ <p className="desc"> En effet, l’indemnisation définitive des préjudices de la victime ne peut se faire que lorsque
+la victime est consolidée. </p>
+
+<p className="desc"> Cela ne signifie pas que la victime est guérie mais que son état de santé est stabilisé c’est-à-
+dire que son état n’est ni susceptible d’amélioration, ni d’aggravation. Tel n’est pas le cas si
+une intervention chirurgicale est programmée ou si des soins sont en cours car l’état sera
+encore susceptible d’évoluer.</p>
+
+<p className="desc"> La phase de consolidation marque donc la séparation entre préjudices temporaires qui sont
+le résultat des lésions et des préjudices permanents qui correspondent à des séquelles. </p>
+
+
+
+
+
+
+
+  <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> REPARATION INTÉGRALE DES PREJUDICES </h2>
+
+
+
+  <p className="desc"> L’indemnisation du dommage corporel des victimes d’accident repose sur le principe de
+réparation intégrale des préjudices de la victime. </p>
+
+
+ <p className="desc"> Selon la formule consacrée, cela implique par le versement de l’indemnisation à replacer la
+victime dans l’état où elle se trouvait si le fait dommageable n’avait pas eu lieu.</p>
+
+
+ <p className="desc"> Le rôle d’un avocat de victime est essentiel pour vous permettre que tous vos préjudices
+soient évalués lors de l’expertise médicale et qu’ils soient bien évalués sur le plan financier.</p>
+
+ <p className="desc"> Pour toute information complémentaire, vous pouvez contacter Maitre Quentin MAMERI,
+avocat de victime à Paris, par téléphone, mail ou par le formulaire de contact afin de lui
+exposer votre situation et convenir d’un rendez-vous.</p>
+
+
+
+
+
+    <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> PREJUDICES INDEMNISABLES </h2>
+
+
+ <p className="desc">  Il est essentiel pour une victime de connaître les préjudices qui sont juridiquement
+indemnisables. </p>
+
+
+<p className="desc"> En effet, il n’est pas rare qu’une victime se présente seule à une expertise médicale ou signe
+un accord d’indemnisation avec l’assureur sans qu’elle ne connaisse véritablement l’étendue
+des préjudices dont elle peut obtenir réparation.</p>
+
+<p className="desc"> Or, quand une victime ne sait pas quel préjudice peut être indemnisé, cela a deux
+conséquences :</p>
+
+
+
+<p className="desc"> - Si l’expert ne l’interroge pas sur un préjudice, celui-ci ne sera pas abordé et sera
+passé sous silence par l’expert ce qui aura pour conséquence que l’assureur ne lui
+fera aucune proposition à ce titre.</p>
+
+
+<p className="desc"> - La victime peut être encline à accepter une sous-indemnisation. Cela n’est pas sans
+conséquence pour l’avenir car il arrive souvent que des préjudices importants soient
+oubliés et que la victime se rendre compte bien plus tard que l’indemnisation qui lui
+a été versée ne compense pas ses besoins.</p>
+
+<p className="desc">
+Les préjudices sont listés par une nomenclature des préjudices « la nomenclature dintilhac »
+qui est adopté par la majorité des juridictions.</p>
+
+<p className="desc">
+Pour toute information complémentaire, vous pouvez contacter Maitre Quentin MAMERI,
+avocat de victime à Paris, par téléphone, mail ou par le formulaire de contact afin de lui
+exposer votre situation et convenir d’un rendez-vous.</p>
+
+
+<p className="desc">  Préjudices de la victime directe et des victimes par ricochet
+Sont indemnisables :
+- les préjudices de la victime directe c’est à dire celle qui a subi le dommage dans sa
+personne ou ses biens
+- les préjudices de la victime dite indirecte c’est-à-dire celles qui a souffert d’un
+préjudice parce qu’une première personne, la victime directe, avec laquelle elle avait
+des liens, a subi un dommage. </p>
+<p className="desc"> Types de préjudice envisageables :
+ Préjudices patrimoniaux et extrapatrimoniaux
+Une victime peut obtenir indemnisation de ses préjudices dits patrimoniaux c’est-à-dire des
+préjudices évaluables sur le plan économiques (pertes de gains professionnels …) et des
+préjudices extrapatrimoniaux c’est-à-dire des préjudices personnels (souffrances endurées,
+préjudice esthétique …).
+ Préjudices temporaires et préjudice permanents
+
+
+Une victime peut obtenir indemnisation de préjudices patrimoniaux ou extrapatrimoniaux
+dit temporaire c’est à dire avant consolidation et des préjudices patrimoniaux ou
+extrapatrimoniaux dits permanents c’est à dire après consolidation. </p>
+
+
+
+
+
+  <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> LE CERTIFICAT MEDICAL INITIAL (CMI)</h2>
+
+
+  <p className="desc"> Le certificat médical initial (CMI) est une pièce fondamentale d’un dossier en réparation du
+dommage corporel.
+Si vous êtes pris en charge par les secours dans les suites de votre accident et hospitalisé,
+l’établissement d’accueil devra établir un certificat médical initial (CMI).
+Ce certificat médical initial fera l’inventaire des blessures constatées et fixera un taux
+d’incapacité.
+Dans le cas où vous ne seriez pas hospitalisé, il est conseillé de vous rendre sans tarder chez
+votre médecin traitant afin que ce dernier puisse certifier des lésions qu’il a lui-même
+constaté suite à son examen clinique.
+Ce certificat est initial est essentiel pour rattacher les lésions à l’accident subi.
+
+Pour toute information complémentaire, vous pouvez contacter Maitre Quentin MAMERI,
+avocat de victime à Paris, par téléphone, mail ou par le formulaire de contact afin de lui
+exposer votre situation et convenir d’un rendez-vous.
+Exemples de certificat médical initial :
+http: </p>
+
+
+ <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="10" height="10" fill="#F2DC17"/>
+  </svg> L'EXPERTISE MEDICALE (CMI)</h2>
+
+
+  <p className="desc"> L’expertise médicale est une étape très importante de la procédure en réparation du
+dommage corporel car er d’évaluer sur le plan médical le préjudice subis par la victime (en
+savoir plus sur les préjudices indemnisables).</p>
+<p className="desc">L’Expertise peut être mise en place dans différents cadres :</p>
+<p className="desc"> Le cadre purement amiable : cela consiste en l’organisation d’une expertise amiable
+avec l’établissement mis en cause ou son assureur.</p>
+<p className="desc"> Le cadre de la Commission de conciliation et d’indemnisation des accidents
+médicaux (uniquement pour les actions en responsabilité médicale) : Cette
+commission a été créée en 2002 par la loi Kouchner. Cette procédure présente
+l’avantage de la gratuité (procédure et frais d’expertise) et d’une certaine rapidité.
+Cependant, la CCI n’est compétente que pour les accidents les plus graves. Le seuil de
+gravité est fixé par décret.</p>
+<p className="desc">  Le cadre judiciaire : ce recours auprès du Tribunal consiste à solliciter la désignation
+d’un expert indépendant. Les frais d’expertises sont à la charge du demandeur mais
+peuvent être pris en charge par une assurance de protection juridique dont vous
+seriez éventuellement titulaire.
+</p>
+<p className="desc"> Pour toute information complémentaire, vous pouvez contacter Maitre Quentin MAMERI,
+avocat de victime à Paris, par téléphone, mail ou par le formulaire de contact afin de lui
+exposer votre situation et convenir d’un rendez-vous.</p>
+
 
   <h2 className="title"> <svg className="yellows" width="15" height="15" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
   <rect width="10" height="10" fill="#F2DC17"/>
@@ -79,7 +253,7 @@ class Reparation extends  Component {
 
         <p className="desc"> - à compter de la consolidation, en cas de séquelles, le versement d’une rente ou d’un capital en fonction du taux d’incapacité retenue. </p>
 
-        <p className="desc"> - l’application de règles protectrices en droit du travail (doublement de l’indemnité de licenciement en cas de licenciement pour inaptitude ; protection contre le licenciement …).   </p>
+        <p className="desc" id="FIE"> - l’application de règles protectrices en droit du travail (doublement de l’indemnité de licenciement en cas de licenciement pour inaptitude ; protection contre le licenciement …).   </p>
 
 
   <p className="desc">En cas de refus de reconnaissance d’un accident du travail ou de désaccord sur votre taux d’IPP, vous pouvez contacter Maitre Quentin MAMERI, par téléphone, mail ou par le formulaire de contact afin de lui exposer votre situation et convenir d’un rendez-vous.   </p>
@@ -100,46 +274,7 @@ class Reparation extends  Component {
 
 </div>
 
-      <div className="rightbox">
-      <div className="contact-box">
-        <div className="contact-left">
-          <img  src={pic2} className="profile-pic-contact" alt="fireSpot" />
-        </div>
-
-        <Link to="/contact/">
-        <div className="contact-right">
-        <h3 className="titleQ"> QUENTIN MAMERI </h3>
-
-
-        <p className="uuup"> 16 rue Sauffroy, 75017 PARIS </p>
-
-
-
-        <p className="uup"> <svg className="yellows" width="13" height="13" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M25.9876 20.519C26.0427 20.9414 25.9141 21.3086 25.6023 21.6207L21.9415 25.2562C21.7764 25.4398 21.5608 25.596 21.2947 25.7244C21.0287 25.853 20.7672 25.9356 20.5104 25.9723C20.4921 25.9723 20.4369 25.977 20.3451 25.9862C20.2535 25.9953 20.1342 26 19.9874 26C19.6387 26 19.0744 25.9403 18.2945 25.8209C17.5147 25.7015 16.5605 25.4077 15.4321 24.9396C14.3035 24.4714 13.0236 23.7689 11.5924 22.8326C10.1612 21.8963 8.63811 20.6109 7.02335 18.9767C5.73888 17.7097 4.6746 16.4979 3.83052 15.3411C2.98644 14.1843 2.30751 13.1147 1.79372 12.1324C1.27988 11.1501 0.894542 10.2595 0.637648 9.46079C0.380754 8.66206 0.206433 7.9735 0.114685 7.39511C0.022937 6.81672 -0.0137622 6.36228 0.0045874 6.03177C0.022937 5.70126 0.0321118 5.51765 0.0321118 5.48092C0.068811 5.22386 0.151384 4.96221 0.279831 4.69597C0.408278 4.42972 0.56425 4.21398 0.747746 4.04872L4.40849 0.385593C4.66538 0.128531 4.95898 0 5.28927 0C5.52781 0 5.73883 0.0688558 5.92233 0.206567C6.10583 0.344279 6.2618 0.514123 6.39024 0.7161L9.33535 6.30719C9.5005 6.60098 9.54637 6.9223 9.47298 7.27117C9.39958 7.62004 9.24361 7.91383 9.00506 8.15253L7.65637 9.5021C7.61967 9.53882 7.58755 9.5985 7.56003 9.68113C7.53251 9.76375 7.51874 9.83261 7.51874 9.88769C7.59214 10.2733 7.75729 10.714 8.01418 11.2097C8.23438 11.6504 8.57385 12.1875 9.03259 12.821C9.49132 13.4544 10.1427 14.1843 10.9868 15.0105C11.8125 15.8552 12.5465 16.5116 13.1888 16.9799C13.8309 17.4479 14.3678 17.7923 14.799 18.0127C15.2302 18.233 15.5605 18.3661 15.7898 18.4119L16.1338 18.4808C16.1705 18.4808 16.2303 18.467 16.3127 18.4395C16.3953 18.4119 16.4549 18.3798 16.4917 18.3431L18.0605 16.7456C18.391 16.4519 18.7762 16.305 19.2166 16.305C19.5286 16.305 19.7762 16.36 19.9597 16.4702H19.9872L25.2994 19.61C25.6848 19.8489 25.9142 20.1518 25.9876 20.519Z" fill="#2a4b80"/>
-      </svg>
-       06 40 84 36 19 </p>
-
-         <p className="uup"> <svg className="yellows3" width="13" height="18" viewBox="0 0 17 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M15.8549 3.01208H15.2476V17.9321C15.2476 18.9232 14.4408 19.7301 13.4496 19.7301H7.2964H2.40332V20.2831C2.40332 20.9165 2.91735 21.4305 3.55068 21.4305H9.70173H15.8528C16.4861 21.4305 17.0001 20.9165 17.0001 20.2831V4.15944C17.0001 3.52612 16.4883 3.01208 15.8549 3.01208Z" fill="#244C82"/>
-<path d="M7.29624 19.0794H13.4473C14.0806 19.0794 14.5946 18.5653 14.5946 17.932V3.01205V1.80831C14.5946 1.17498 14.0806 0.66095 13.4473 0.66095H7.29624H5.25095V1.04051C5.25526 1.07955 5.25745 1.12076 5.25745 1.16197V3.66273V4.30473C5.25745 5.19615 4.53304 5.92057 3.64161 5.92057H2.99961H0.498851C0.466317 5.92057 0.431614 5.9184 0.399081 5.91623H0V17.932C0 18.5653 0.514033 19.0794 1.14736 19.0794H2.40533H7.29624ZM7.52831 15.1797H3.51798C3.21867 15.1797 2.97575 14.9367 2.97575 14.6374C2.97575 14.3381 3.21867 14.0952 3.51798 14.0952H7.53048C7.82979 14.0952 8.07271 14.3381 8.07271 14.6374C8.07271 14.9367 7.82762 15.1797 7.52831 15.1797ZM11.5408 12.4122H3.51798C3.21867 12.4122 2.97575 12.1692 2.97575 11.8699C2.97575 11.5706 3.21867 11.3277 3.51798 11.3277H11.5408C11.8401 11.3277 12.083 11.5706 12.083 11.8699C12.083 12.1692 11.8401 12.4122 11.5408 12.4122ZM3.51798 8.38879H11.5408C11.8401 8.38879 12.083 8.63171 12.083 8.93102C12.083 9.23033 11.8401 9.47325 11.5408 9.47325H3.51798C3.21867 9.47325 2.97575 9.23033 2.97575 8.93102C2.97575 8.63171 3.21867 8.38879 3.51798 8.38879Z" fill="#244C82"/>
-<path d="M0.498756 5.26988H3.64152C3.64369 5.26988 3.64802 5.26988 3.65019 5.26988C4.17507 5.26554 4.60018 4.84043 4.60452 4.31555C4.60452 4.31338 4.60452 4.30905 4.60452 4.30688V1.16195C4.60452 0.871317 4.36593 0.678284 4.1187 0.678284C3.99941 0.678284 3.88012 0.721662 3.78035 0.821432L0.156067 4.44569C-0.147581 4.74934 0.0671417 5.26988 0.498756 5.26988Z" fill="#244C82"/>
-</svg>01 84 10 55 86
-</p>
-
-        <p className="uup" > <svg className="yellows2" width="18" height="11" viewBox="0 0 27 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M2.59146 5.88196C2.93301 6.09875 3.96255 6.74264 5.68015 7.81327C7.3978 8.88391 8.71364 9.70828 9.62772 10.2864C9.72814 10.3498 9.9415 10.4875 10.2679 10.6999C10.5944 10.9124 10.8656 11.0841 11.0814 11.215C11.2974 11.3459 11.5586 11.4928 11.8651 11.6554C12.1715 11.8178 12.4603 11.94 12.7315 12.0209C13.0028 12.1025 13.2538 12.1429 13.4848 12.1429H13.5H13.5152C13.7462 12.1429 13.9974 12.1024 14.2686 12.0209C14.5397 11.94 14.8288 11.8177 15.1349 11.6554C15.4412 11.4926 15.7023 11.3459 15.9183 11.215C16.1343 11.0841 16.4054 10.9124 16.7319 10.6999C17.0583 10.4874 17.2719 10.3498 17.3723 10.2864C18.2963 9.70828 20.6469 8.23998 24.4236 5.88167C25.1568 5.42107 25.7694 4.86529 26.2615 4.21472C26.754 3.56443 27 2.88224 27 2.16852C27 1.57211 26.7613 1.06157 26.2841 0.636952C25.807 0.212238 25.2419 0 24.5892 0H2.41061C1.63722 0 1.04205 0.234882 0.6252 0.704645C0.2084 1.1745 0 1.7618 0 2.46649C0 3.03571 0.276319 3.65253 0.828692 4.31659C1.38101 4.98069 1.9688 5.50253 2.59146 5.88196Z" fill="#2a4b80"/>
-      <path d="M25.4932 7.33201C22.1989 9.33767 19.6975 10.8964 17.9901 12.0079C17.4176 12.3872 16.9531 12.6833 16.5964 12.8955C16.2397 13.1078 15.7654 13.3247 15.1726 13.5459C14.5801 13.7675 14.0279 13.878 13.5155 13.878H13.5001H13.485C12.9727 13.878 12.4202 13.7675 11.8277 13.5459C11.2351 13.3247 10.7604 13.1078 10.4039 12.8955C10.0474 12.6833 9.58277 12.3872 9.01029 12.0079C7.65419 11.1134 5.15814 9.5546 1.52204 7.33201C0.949347 6.9889 0.442094 6.59561 0.00012207 6.15295V16.9137C0.00012207 17.5104 0.236069 18.0206 0.708228 18.4453C1.18028 18.8701 1.74791 19.0824 2.41089 19.0824H24.5895C25.2523 19.0824 25.8199 18.8701 26.292 18.4453C26.7643 18.0205 27.0001 17.5104 27.0001 16.9137V6.15295C26.5681 6.58645 26.066 6.97974 25.4932 7.33201Z" fill="#2a4b80"/>
-      </svg>
-       qmameri.avocat@gmail.com</p>
-
-        </div>
-         </Link>
-
-
-      </div>
-
-      </div>
+     <VisitCard />
     </div>
 
 
