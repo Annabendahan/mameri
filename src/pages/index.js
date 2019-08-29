ecc31ed969ca677b9cc45d58fff5234f49115336
Anna Bendahan <an.bendahan@gmail.com>
2019-08-29 15:19:41 +0200

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ Me MAMERI reçoit uniquement sur rendez-vous. Si vous n’êtes pas en capacité
   <h3> DÉFENSE DES VICTIMES (INDEMNISATION) </h3>
   <p>
   Vous êtes victime d’une agression ou d’un accident? Maître Quentin MAMERI
-  vous accompagne à tous les stades de la procédure  pour obtenir
+  assure votre défense et vous accompagne à tous les stades de la procédure  pour obtenir
   la réparation intégrale de vos préjudices.
   </p>
 
@@ -83,7 +83,7 @@ Me MAMERI reçoit uniquement sur rendez-vous. Si vous n’êtes pas en capacité
    <img  src={pic5} className="picindex2" alt="fireSpot" />
   <h3> DÉFENSE DES MALADES</h3>
   <p>
-  Vous avez un litige concernant des prestations sociales? Me MAMERI vous représentera et vous assistera dans l’ensemble des litiges de sécurité sociale et d’aide sociale en matière de santé et de handicap.</p>
+  Vous avez un litige concernant des prestations sociales? Me MAMERI assurera votre défense, vous représentera et vous assistera dans l’ensemble des litiges de sécurité sociale et d’aide sociale en matière de santé et de handicap.</p>
    <p className="lire"> En savoir plus  <svg className="arrow" width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M13.3536 4.35355C13.5488 4.15829 13.5488 3.84171 13.3536 3.64645L10.1716 0.464466C9.97631 0.269204 9.65973 0.269204 9.46447 0.464466C9.2692 0.659728 9.2692 0.976311 9.46447 1.17157L12.2929 4L9.46447 6.82843C9.2692 7.02369 9.2692 7.34027 9.46447 7.53553C9.65973 7.7308 9.97631 7.7308 10.1716 7.53553L13.3536 4.35355ZM0 4.5L13 4.5V3.5L0 3.5L0 4.5Z" fill="white"/>
 </svg>  </p>
