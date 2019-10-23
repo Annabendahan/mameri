e4e78cd48500cd3f9b5232eee8a2093d1853e993
Anna Bendahan <anna@blacksmith.studio>
2019-10-23 17:24:43 +0200

    change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,12 @@
-require('regenerator-runtime/runtime')
-require('dotenv').config({
-  path: '.env',
+require("dotenv").config({
+  path: `.env`
 });
-const path = require('path');
-
 
 module.exports = {
-  pathPrefix: `/mameri`,
   siteMetadata: {
-    title: `quentinmameri`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `For fun...`,
+    description: `...and more fun!`,
+    author: `@n8finch`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +14,37 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
+         */
+        baseUrl: `09b674e0.ngrok.io`,
+        // The protocol. This can be http or https.
+        protocol: `http`,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
+        // This feature is untested for sites hosted on WordPress.com
+        useACF: false,
+        includedRoutes: [
+          "**/posts",
+          "**/pages",
+        ],
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-purgecss`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -32,44 +54,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-source-wordpress`,
-      options: {
-        /*
-         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
-         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
-         */
-        baseUrl: "a752ae1f.ngrok.io",
-        // The protocol. This can be http or https.
-        protocol: "http",
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the assumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: false,
-         auth: {
-        wpcom_app_clientSecret: 'Bkbg9upbqm2bNUAoQjPFxCzkMM0D3gGKdu23ws2YtokHNUbzgnNh0zTvWnasoxRP',
-        wpcom_app_clientId: '66249',
-        wpcom_user: 'quentinmameri17',
-        wpcom_pass: '17mameri17',
-      },
-        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
-        // This feature is untested for sites hosted on wordpress.com.
-        // Defaults to true.
-        useACF: true,
-        verboseOutput: true
-
+        icon: `src/images/logo.svg` // This path is relative to the root of the site.
       }
     },
-
-
-
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    "gatsby-plugin-offline",
+    `gatsby-plugin-netlify` // make sure to put last in the array
+  ]
+};
